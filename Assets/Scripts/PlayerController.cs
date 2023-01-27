07612b94d59cbe9c18d05742ce35c336906742be
Acetorrent <phihoangkhai2811@gmail.com>
2023-01-27 00:09:26 -0500

    Custom UI element
    - Create a UI
    - Adjust Canvas and UI placement
    - Add UI interaction using TextMeshPro
    - Create a public variable that can be changed in the Editor.
    - Update the EventSystem from InputTaskmanager to new System Input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
 
     public float speed = 0;
+
+    public TextMeshProUGUI countText;
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
     }
     void OnMove(InputValue movementValue)
     {
@@ -24,6 +29,13 @@ public class PlayerController : MonoBehaviour
 
         movementX= movementVector.x;
         movementY= movementVector.y;
+
+        SetCountText();
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString(); ;
     }
 
     void FixedUpdate()
@@ -38,6 +50,9 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
         }
     }
 }
